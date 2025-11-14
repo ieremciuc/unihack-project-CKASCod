a1002cafce131f527e4d0f0d01605aa7f8cdf154
Ieremciuc Ciprian Stefan <ieremciuc.ciprian@gmail.com>
2025-11-14 19:15:06 +0200

    Railway #2

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-export const CLOUDINARY_API_URL = "https://cloudinary-api-production-f738.up.railway.app";
+export const CLOUDINARY_API_URL = "https://cloudinary-api-production-8da8.up.railway.app";
 export const CLOUDINARY_URL = "https://res.cloudinary.com/dsmrwftcx/image/upload/"
